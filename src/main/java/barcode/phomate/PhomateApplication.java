a43da680093ef6c677b8951d98006e936bb258ad
dev-cyan <zyos1@naver.com>
2025-11-20 16:21:18 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/barcode/phomate/PhomateApplication.java
@@ -0,0 +1,13 @@
+package barcode.phomate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhomateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhomateApplication.class, args);
+	}
+
+}
