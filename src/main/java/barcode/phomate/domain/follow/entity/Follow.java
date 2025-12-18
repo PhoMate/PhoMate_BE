74aae1e997aabdadbeb3a53dd4995ce3370832cb
chohaewon48 <chocynthia@naver.com>
2025-12-18 15:10:59 +0900

    feat: Follow 엔티티 구현

---
+++ src/main/java/barcode/phomate/domain/follow/entity/Follow.java
@@ -0,0 +1,43 @@
+package barcode.phomate.domain.follow.entity;
+
+import barcode.phomate.domain.member.domain.entity.Member;
+import barcode.phomate.global.common.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Follow extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "follower_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member follower;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "followee_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member followee;
+
+    @Builder
+    public Follow(Member follower, Member followee) {
+        this.follower = follower;
+        this.followee = followee;
+    }
+}
