8fc3caa2fad754f68c26db53cc96b8916f435eb6
dev-cyan <zyos1@naver.com>
2025-12-18 12:32:28 +0900

    feat: Member 엔티티 추가

---
+++ src/main/java/barcode/phomate/domain/member/domain/entity/Member.java
@@ -0,0 +1,29 @@
+package barcode.phomate.domain.member.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String nickname;
+
+    @Column(length = 2048)
+    private String profileImageUrl;
+
+    @Builder
+    public Member(String nickname, String profileImageUrl) {
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+    }
+}
