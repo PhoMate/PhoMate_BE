7e4743f684d6d60c6526c5c14ce1741410c880f4
dev-cyan <zyos1@naver.com>
2025-12-18 12:34:31 +0900

    feat: BaseEntity 추가

--- src/main/java/barcode/phomate/PhomateApplication.java
+++ src/main/java/barcode/phomate/PhomateApplication.java
@@ -2,8 +2,10 @@ package barcode.phomate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PhomateApplication {
 
 	public static void main(String[] args) {
