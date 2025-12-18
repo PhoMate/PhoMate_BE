c0e43c929fbf61f2f5a80d52ee252d289f9a6764
chohaewon48 <chocynthia@naver.com>
2025-12-18 15:19:20 +0900

    feat: ChatSession 엔티티 구현

---
+++ src/main/java/barcode/phomate/domain/chat/entity/ChatMessage.java
@@ -0,0 +1,12 @@
+package barcode.phomate.domain.chat.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatMessage {
+}
---
+++ src/main/java/barcode/phomate/domain/chat/entity/ChatSession.java
@@ -0,0 +1,39 @@
+package barcode.phomate.domain.chat.entity;
+
+import barcode.phomate.domain.member.domain.entity.Member;
+import barcode.phomate.global.common.entity.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "chat_session")
+public class ChatSession extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "member_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Member member;
+
+    @Builder
+    public ChatSession(Member member) {
+        this.member = member;
+    }
+}
