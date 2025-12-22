649708fb46569c1d143ef2d43dd24a53b688bd87
dev-cyan <zyos1@naver.com>
2025-12-22 15:49:56 +0900

    feat: 회원 정보 조회 API 추가

--- src/main/java/barcode/phomate/domain/member/domain/api/MemberController.java
+++ src/main/java/barcode/phomate/domain/member/domain/api/MemberController.java
@@ -1,13 +1,27 @@
 package barcode.phomate.domain.member.domain.api;
 
+import barcode.phomate.domain.member.domain.application.MemberService;
+import barcode.phomate.domain.member.dto.MemberResponseDTO;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/members")
 @RequiredArgsConstructor
 @Tag(name = "멤버 API", description = "멤버 API입니다.")
 public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/{memberId}")
+    @Operation(summary = "회원 정보 조회", description = "회원 정보 조회 API")
+    public ResponseEntity<MemberResponseDTO> getMember(@PathVariable Long memberId) {
+        return ResponseEntity.ok(memberService.getMember(memberId));
+    }
 }
--- src/main/java/barcode/phomate/domain/member/domain/application/MemberServiceImpl.java
+++ src/main/java/barcode/phomate/domain/member/domain/application/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package barcode.phomate.domain.member.domain.application;
 
+import barcode.phomate.domain.member.domain.entity.Member;
+import barcode.phomate.domain.member.domain.repository.MemberRepository;
+import barcode.phomate.domain.member.dto.MemberResponseDTO;
+import barcode.phomate.global.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +12,15 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public MemberResponseDTO getMember(Long memberId) {
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new NotFoundException("멤버가 존재하지 않습니다."));
+
+        return MemberResponseDTO.from(member);
+    }
 }
