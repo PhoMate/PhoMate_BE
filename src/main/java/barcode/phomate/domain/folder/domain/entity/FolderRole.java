26bffb735c2870a74e2053ee97c8bab537cc93c9
chohaewon48 <chocynthia@naver.com>
2026-02-22 19:46:05 +0900

    feat: 폴더 관련 엔티티 구현

---
+++ src/main/java/barcode/phomate/domain/folder/domain/entity/FolderRole.java
@@ -0,0 +1,7 @@
+package barcode.phomate.domain.folder.domain.entity;
+
+public enum FolderRole {
+    READ, // 읽기
+    WRITE, // 쓰기
+    ADMIN // 관리자
+}
---
+++ src/main/java/barcode/phomate/domain/folder/domain/entity/FolderType.java
@@ -0,0 +1,6 @@
+package barcode.phomate.domain.folder.domain.entity;
+
+public enum FolderType {
+    PERSONAL, // 일반 폴더
+    SHARED // 공유 폴더
+}
