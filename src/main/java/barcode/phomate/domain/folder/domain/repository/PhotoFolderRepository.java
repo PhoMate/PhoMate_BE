4a86e2231ed791208b50eb7aeb65ac21dc2da43c
chohaewon48 <chocynthia@naver.com>
2026-02-22 20:19:21 +0900

    feat: 폴더 repository 추가

---
+++ src/main/java/barcode/phomate/domain/folder/domain/repository/PhotoFolderRepository.java
@@ -0,0 +1,21 @@
+package barcode.phomate.domain.folder.domain.repository;
+
+import barcode.phomate.domain.folder.domain.entity.Folder;
+import barcode.phomate.domain.folder.domain.entity.PhotoFolder;
+import barcode.phomate.domain.photo.domain.entity.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PhotoFolderRepository extends JpaRepository<PhotoFolder, Long> {
+
+    // 폴더 안 사진 조회(폴더 상세 조회)
+    List<PhotoFolder> findByFolder(Folder folder);
+
+    // 중복 추가 방지 (사진을 폴더에 추가할때)
+    Optional<PhotoFolder> findByPhotoAndFolder(Photo photo, Folder folder);
+
+    // 사진 삭제 시 연관 PhotoFolder 레코드 정리
+    void deleteByPhoto(Photo photo);
+}
